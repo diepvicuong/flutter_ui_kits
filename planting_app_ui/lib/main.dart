4b041235ae765bf79f894ba938651cb9600012ab
diepvicuong97 <diepvicuong97@gmail.com>
2021-04-03 22:18:16 +0700

    Add animation

--- planting_app_ui/lib/main.dart
+++ planting_app_ui/lib/main.dart
@@ -1,9 +1,8 @@
-import 'package:dribbble_plant_app/models/plant_item.dart';
 import 'package:dribbble_plant_app/widgets/left_navigation_bar.dart';
-import 'package:dribbble_plant_app/widgets/plant_widget.dart';
 import 'package:flutter/material.dart';
 
 import 'models/drawer_item.dart';
+import 'pages/home_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -36,83 +35,37 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Row(
+          child: Stack(
             children: [
               // NavigationRail(destinations: destinations, selectedIndex: selectedIndex)
-              LeftNavigationBar(
-                currentIndex: _currentIndex,
-                items: _drawerItems,
-                width: 70,
-                onTap: (value) {
-                  setState(() {
-                    print("Current index: $value");
-                    _currentIndex = value;
-                  });
-                },
-                topIcon: Icon(
-                  Icons.workspaces_filled,
-                  color: Colors.white,
-                ),
-                onTapTopIcon: () {
-                  print("onTapTopIcon");
-                },
-                bottomIcon: Icon(Icons.home_outlined, color: Colors.white),
-                onTapBottomIcon: () {
-                  print("onTapBottomIcon");
-                },
-              ),
-              Expanded(
-                  child: ConstrainedBox(
-                constraints: BoxConstraints.expand(),
-                child: SingleChildScrollView(
-                  child: Column(
-                    mainAxisSize: MainAxisSize.max,
-                    // mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: [
-                      Align(
-                        alignment: Alignment.centerRight,
-                        child: IconButton(
-                            icon: Icon(
-                              Icons.search,
-                            ),
-                            onPressed: () {}),
-                      ),
-                      Container(
-                        margin: EdgeInsets.only(left: 40),
-                        child: RichText(
-                          text: TextSpan(
-                            text: 'Green ',
-                            style: TextStyle(color: Colors.black),
-                            children: <TextSpan>[
-                              TextSpan(text: '\n'),
-                              TextSpan(
-                                  text: 'Plant',
-                                  style: TextStyle(
-                                      fontSize: 40,
-                                      fontWeight: FontWeight.bold)),
-                            ],
-                          ),
-                        ),
-                      ),
-                      PlantWidget(
-                          plant: PlantItem(
-                              name: "Turf pot plant",
-                              description:
-                                  "Big leaf plant in a turf pot for your home or office decor",
-                              price: 45.00,
-                              image: 'assets/images/klipartz.png')),
-                      PlantWidget(
-                          plant: PlantItem(
-                              name: "Scandinavian plant",
-                              description:
-                                  "Low maintenance flower in a white ceramic pot",
-                              price: 45.00,
-                              image: 'assets/images/scandinavian.png')),
-                    ],
+              Container(
+                color: Colors.white,
+                child: LeftNavigationBar(
+                  currentIndex: _currentIndex,
+                  items: _drawerItems,
+                  width: 70,
+                  onTap: (value) {
+                    setState(() {
+                      print("Current index: $value");
+                      _currentIndex = value;
+                    });
+                  },
+                  topIcon: Icon(
+                    Icons.workspaces_filled,
+                    color: Colors.white,
                   ),
+                  onTapTopIcon: () {
+                    print("onTapTopIcon");
+                  },
+                  bottomIcon: Icon(Icons.home_outlined, color: Colors.white),
+                  onTapBottomIcon: () {
+                    print("onTapBottomIcon");
+                  },
                 ),
-              ))
+              ),
+              HomeScreen(
+                index: _currentIndex,
+              ),
             ],
           ),
         ),
---
+++ planting_app_ui/lib/pages/home_screen.dart
@@ -0,0 +1,94 @@
+import 'package:dribbble_plant_app/models/plant_item.dart';
+import 'package:dribbble_plant_app/widgets/plant_widget.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  final int index;
+  const HomeScreen({
+    Key? key,
+    required this.index,
+  }) : super(key: key);
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  double xOffset = 0;
+  double yOffset = 0;
+  bool isDrawerOpen = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedContainer(
+      // transform: Matrix4.translationValues(xOffset, yOffset, 0)
+      //   ..rotateY(isDrawerOpen ? -0.5 : 0),
+      duration: Duration(milliseconds: 200),
+      decoration: BoxDecoration(
+          color: widget.index == 0 ? Colors.white : Colors.blue[100]),
+      height: double.infinity,
+      margin: EdgeInsets.only(left: isDrawerOpen ? 70 : 0),
+      child: SingleChildScrollView(
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+              isDrawerOpen
+                  ? IconButton(
+                      icon: Icon(Icons.arrow_back),
+                      onPressed: () {
+                        setState(() {
+                          xOffset = 0;
+                          yOffset = 0;
+                          isDrawerOpen = false;
+                        });
+                      },
+                    )
+                  : IconButton(
+                      icon: Icon(Icons.menu),
+                      onPressed: () {
+                        setState(() {
+                          xOffset = 70;
+                          yOffset = 0;
+                          isDrawerOpen = true;
+                        });
+                      }),
+              IconButton(icon: Icon(Icons.search), onPressed: () {}),
+            ]),
+            Container(
+              margin: EdgeInsets.only(left: 40),
+              child: RichText(
+                text: TextSpan(
+                  text: 'Green ',
+                  style: TextStyle(color: Colors.black),
+                  children: <TextSpan>[
+                    TextSpan(text: '\n'),
+                    TextSpan(
+                        text: 'Plant',
+                        style: TextStyle(
+                            fontSize: 40, fontWeight: FontWeight.bold)),
+                  ],
+                ),
+              ),
+            ),
+            PlantWidget(
+                plant: PlantItem(
+                    name: "Turf pot plant",
+                    description:
+                        "Big leaf plant in a turf pot for your home or office decor",
+                    price: 45.00,
+                    image: 'assets/images/klipartz.png')),
+            PlantWidget(
+                plant: PlantItem(
+                    name: "Scandinavian plant",
+                    description:
+                        "Low maintenance flower in a white ceramic pot",
+                    price: 45.00,
+                    image: 'assets/images/scandinavian.png')),
+          ],
+        ),
+      ),
+    );
+  }
+}
